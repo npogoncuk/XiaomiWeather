7abf61e119df27a0d3e8d464b29ba69bc31d30f4
npogo <npogoncuk@gmail.com>
2024-06-02 14:22:20 +0300

    Add SettingsScreen.kt preview

--- app/src/main/java/com/nazar/petproject/xiaomiweather/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/nazar/petproject/xiaomiweather/ui/screens/settings/SettingsScreen.kt
@@ -8,9 +8,12 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.nazar.petproject.xiaomiweather.ui.screens.settings.components.SettingMediumTopAppBar
 import com.nazar.petproject.xiaomiweather.ui.screens.settings.components.SettingsContent
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,4 +30,10 @@ fun SettingsScreen(navController: NavController) {
     ) { paddingValues ->
         SettingsContent(modifier = Modifier.padding(paddingValues))
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsScreenPreview() {
+    SettingsScreen(navController = rememberNavController())
 }
